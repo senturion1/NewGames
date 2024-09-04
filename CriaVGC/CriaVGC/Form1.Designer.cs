428029cd8ef67ca105189a586980aeab2291232b
senturion1 <116207350+senturion1@users.noreply.github.com>
2024-09-04 16:56:25 -0300

    Add files via upload

---
+++ CriaVGC/CriaVGC/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace CriaVGC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxInicial2 = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cbxInicial1 = new System.Windows.Forms.ComboBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbxInicial2
+            // 
+            this.cbxInicial2.FormattingEnabled = true;
+            this.cbxInicial2.Items.AddRange(new object[] {
+            "Charmander",
+            "Totodaio",
+            "Sprigatito"});
+            this.cbxInicial2.Location = new System.Drawing.Point(667, 59);
+            this.cbxInicial2.Name = "cbxInicial2";
+            this.cbxInicial2.Size = new System.Drawing.Size(121, 21);
+            this.cbxInicial2.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(292, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 40);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cbxInicial1
+            // 
+            this.cbxInicial1.FormattingEnabled = true;
+            this.cbxInicial1.Items.AddRange(new object[] {
+            "Charmander",
+            "Totodaio",
+            "Sprigatito"});
+            this.cbxInicial1.Location = new System.Drawing.Point(12, 59);
+            this.cbxInicial1.Name = "cbxInicial1";
+            this.cbxInicial1.Size = new System.Drawing.Size(121, 21);
+            this.cbxInicial1.TabIndex = 1;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(352, 269);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(35, 13);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbxInicial2);
+            this.Controls.Add(this.cbxInicial1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxInicial2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cbxInicial1;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ CriaVGC/CriaVGC/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CriaVGC
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+        public void danoGolpe() 
+        {
+            lblTotal.Text = "";
+
+            Random rand = new Random();
+            Random rand2 = new Random();
+            double nivel = 5, Atk = 50, Def = 60, Power = 20, delta = 0, Stab = 2, Crit = Convert.ToDouble(rand2.Next(1, 4)), Efetive = 2, Addons = 1, dano = 0;
+
+            //double Atk = 50;
+            //double Def = 60;
+            //double Power = 20;
+            //double nivel = 5;
+            //double Addons = 1;
+            //double Stab = 2;
+            double vlrRan = rand.Next(85, 100);
+            double RandomVlr = Convert.ToDouble( vlrRan * 0.01);
+
+            if (Crit != 4)
+            {
+                Crit = 1;
+            }
+            else
+            {
+                Crit = Atk * 0.4;
+            }
+            double calDelta = Stab * Efetive * Crit * Addons * RandomVlr;
+
+
+             dano = ( ((2 * nivel + 10) / 50) * ((Atk / Def) * Power + 2)) * calDelta;
+
+
+            lblTotal.Text = Convert.ToString(Math.Round(dano));
+        }
+
+        public void btnStart_Click(object sender, EventArgs e)
+        {
+            
+
+            danoGolpe();
+
+        }
+    }
+}
